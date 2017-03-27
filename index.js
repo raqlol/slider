29111ea6b2bd4b7c98a3856ec6dd47dad4ebcfac
raqlol <raquel.lowell@gmail.com>
2017-03-27 11:13:29 -0700

    basic slide working

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 'use strict';
 var slider=document.getElementById('sliderList').childNodes
+var index = slider.length-1
+console.log(slider.length)
 function slide() {
-  console.log(slider)
+  console.log(index, slider.length)
+  if(index < slider.length && index > 0) {
+    slider[index].className='slide'
+    console.log(slider[index]);
+    index -=1;
+  }
+  else if (index == 0) {
+    index = slider.length - 1
+    console.log('reset');
+    for(let img of slider) {
+      img.classList.remove("slide")
+    }
+  }
 }
-slide();
+
+function autoSlide() {
+  setInterval(slide, 5000)
+}
+autoSlide();
