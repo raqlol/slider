c8246e466226d6e07c6572f144c5df31c1669723
raquel <raquel.lowell@gmail.com>
2017-03-28 13:14:45 -0700

    right arrow functionality

--- index.js
+++ index.js
@@ -1,48 +1,86 @@
 'use strict';
+//slider images
 var slider=document.getElementById('sliderList').childNodes;
+//image progression
 var mainImg = slider.length - 1;
 var leftImg = 0;
 var rightImg = mainImg - 1;
-
-function moveLeft(){
-  console.log('WORKS');
+//auto progression
+var autoInterval = setInterval(slideControl, 5000);
+console.log('running auto slider - horray!')
+//controls for changing the images
+function setDefaultProg() {
+  leftImg -= 1;
+  mainImg -=1;
+  rightImg -=1;
 }
+function setLeftReset() {
+  leftImg = slider.length - 1;
+  mainImg -=1;
+  rightImg -=1;
+}
+function setMainReset() {
+  leftImg -= 1;
+  mainImg = slider.length - 1;
+  rightImg -= 1;
+}
+function setRightReset() {
+  leftImg -= 1;
+  mainImg -= 1;
+  rightImg = slider.length - 1;
+}
+//left right button actions
 function moveRight() {
-  console.log('WORKS');
+  clearInterval(autoInterval);
+  console.log('you stopped the auto slider ... the fate of this slider is in your hands')
+  if (leftImg == 0){
+    instantSlide();
+    setLeftReset()
+  }
+  else if (rightImg == 0){
+    instantSlide()
+    setRightReset()
+  }
+  else if (mainImg == 0){
+    instantSlide()
+    setMainReset()
+  }
+  else {
+    instantSlide()
+    setDefaultProg()
+  }
+  autoInterval = setInterval(slideControl, 10000);
 }
-function autoSlide() {
-  setInterval(slideControl, 3000);
-}
+//auto play slide actions
 function slideControl (){
+  console.log('auto slider is running again')
   if (leftImg == 0) {
     slide();
-    leftImg = slider.length - 1;
-    mainImg -=1;
-    rightImg -=1;
+    setLeftReset()
   }
   else if (rightImg == 0){
     slide()
-    leftImg -= 1;
-    mainImg -= 1;
-    rightImg = slider.length - 1;
+    setRightReset()
   }
   else if (mainImg == 0){
     slide()
-    leftImg -= 1;
-    mainImg = slider.length - 1;
-    rightImg -= 1;
+    setMainReset()
   }
   else {
     slide()
-    leftImg -= 1;
-    mainImg -=1;
-    rightImg -=1;
+    setDefaultProg();
   }
 };
+// slide speeds for auto and manual
 function slide() {
   slider[rightImg].classList.remove("start-right");
   slider[mainImg].classList.add("slide-left");
-  setTimeout(function(){slider[leftImg].classList.add("start-right");}, 1000);
-  setTimeout(function(){slider[leftImg].classList.remove("slide-left");}, 2000);
+  setTimeout(function(){slider[leftImg].classList.add("start-right");}, 100);
+  setTimeout(function(){slider[leftImg].classList.remove("slide-left");}, 1000);
 }
-autoSlide();
+function instantSlide() {
+  slider[rightImg].classList.remove("start-right");
+  slider[mainImg].classList.add("slide-left");
+  slider[leftImg].classList.add("start-right");
+  slider[leftImg].classList.remove("slide-left");
+}
