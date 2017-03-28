225cd44fd8c9e76ea7aa7050d3152cbc4657c160
raqlol <raquel.lowell@gmail.com>
2017-03-28 11:45:28 -0700

    arrows that click and go no where

--- index.js
+++ index.js
@@ -4,37 +4,35 @@ var mainImg = slider.length - 1;
 var leftImg = 0;
 var rightImg = mainImg - 1;
 
+function moveLeft(){
+  console.log('WORKS');
+}
+function moveRight() {
+  console.log('WORKS');
+}
 function autoSlide() {
-  setInterval(slideControl, 15000);
+  setInterval(slideControl, 3000);
 }
 function slideControl (){
   if (leftImg == 0) {
-  console.log("first if");
     slide();
-    console.log(leftImg,mainImg,rightImg);
     leftImg = slider.length - 1;
     mainImg -=1;
     rightImg -=1;
   }
   else if (rightImg == 0){
-    console.log("ELSE 1!!");
-    console.log(leftImg,mainImg,rightImg);
     slide()
     leftImg -= 1;
     mainImg -= 1;
     rightImg = slider.length - 1;
   }
   else if (mainImg == 0){
-    console.log("ELSE 2!!");
-    console.log(leftImg,mainImg,rightImg);
     slide()
     leftImg -= 1;
     mainImg = slider.length - 1;
     rightImg -= 1;
   }
   else {
-    console.log("ELSE 3!");
-    console.log(leftImg,mainImg,rightImg);
     slide()
     leftImg -= 1;
     mainImg -=1;
@@ -42,16 +40,9 @@ function slideControl (){
   }
 };
 function slide() {
-  console.log('STARTING');
-  console.log("right img " + rightImg);
-  console.log(slider[rightImg]);
-  console.log("main img " + mainImg);
-  console.log(slider[mainImg]);
-  console.log("left img " + leftImg);
-  console.log(slider[leftImg]);
   slider[rightImg].classList.remove("start-right");
   slider[mainImg].classList.add("slide-left");
-  setTimeout(function(){slider[leftImg].classList.add("start-right");console.log("MOVE BACK TO RIGHT WORKS")}, 1000);
-  setTimeout(function(){slider[leftImg].classList.remove("slide-left");console.log("MOVE BACK TO RIGHT complete?")}, 2000);
+  setTimeout(function(){slider[leftImg].classList.add("start-right");}, 1000);
+  setTimeout(function(){slider[leftImg].classList.remove("slide-left");}, 2000);
 }
 autoSlide();
